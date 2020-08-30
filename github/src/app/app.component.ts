ae56b7044d7f250f93457cf7e26ab85da39b0928
Irene-nandy <irenekasiva75@gmail.com>
2020-08-30 21:07:35 +0300

    add app routing module

--- github/src/app/app.component.ts
+++ github/src/app/app.component.ts
@@ -1,13 +1,13 @@
-// import { Component } from '@angular/core';
-// import {GitsearcherService} from 'src/app/gitsearcher.service'
-// import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
+import { Component } from '@angular/core';
+import {GitsearcherService} from 'src/app/gitsearcher.service'
+import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
 
 
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Gitsearch';
-// }
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Gitsearch';
+}
