fc9462b3e297a6630e72922e9f11ed2c1ee2c347
Irene-nandy <irenekasiva75@gmail.com>
2020-08-30 21:03:13 +0300

    add repositories changes

--- github/src/app/repositories/repositories.component.ts
+++ github/src/app/repositories/repositories.component.ts
@@ -1,36 +1,36 @@
-// import { Component, OnInit } from '@angular/core';
-// import {Repository} from 'src/app/repository';
-// import {Users} from 'src/app/users';
-// import { GitsearcherService } from 'src/app/gitsearcher.service'
-// import { GitsearchFormComponent } from 'src/app/gitsearch-form/gitsearch-form.component';
-// import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
-// import { AppComponent } from 'src/app/app.component';
-// import {FormsModule} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {Repository} from 'src/app/repository';
+import {Users} from 'src/app/users';
+import { GitsearcherService } from 'src/app/gitsearcher.service'
+import { GitsearchFormComponent } from 'src/app/gitsearch-form/gitsearch-form.component';
+import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
+import { AppComponent } from 'src/app/app.component';
+import {FormsModule} from '@angular/forms';
 
 
-// @Component({
-//   selector: 'app-repositories',
-//   templateUrl: './repositories.component.html',
-//   styleUrls: ['./repositories.component.css']
-// })
-// export class RepositoriesComponent implements OnInit {
-//   details: any;
-//   repos: any;
-//   username:string;
+@Component({
+  selector: 'app-repositories',
+  templateUrl: './repositories.component.html',
+  styleUrls: ['./repositories.component.css']
+})
+export class RepositoriesComponent implements OnInit {
+  details: any;
+  repos: any;
+  username:string;
 
 
-//   constructor(private gitsearcherService: GitsearcherService) { }
+  constructor(private gitsearcherService: GitsearcherService) { }
 
-//   ngOnInit() {
-//   }
-// findUser(){
+  ngOnInit() {
+  }
+findUser(){
 
-//   this.gitsearcherService.getRepoInfo().subscribe(repos => {
-//     console.log(repos);
-//     this.repos = repos;
-// })
+  this.gitsearcherService.getRepoInfo().subscribe(repos => {
+    console.log(repos);
+    this.repos = repos;
+})
 
-// }
-// }
+}
+}
 
 
