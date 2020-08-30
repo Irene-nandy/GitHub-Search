f9a36ab4c00eeb5ad81bd16c7472fea7248b10c3
Irene-nandy <irenekasiva75@gmail.com>
2020-08-30 20:59:30 +0300

    add git search component spec

--- github/src/app/gitsearch/gitsearch.component.spec.ts
+++ github/src/app/gitsearch/gitsearch.component.spec.ts
@@ -1,25 +1,25 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { GitsearchComponent } from './gitsearch.component';
+import { GitsearchComponent } from './gitsearch.component';
 
-// describe('GitsearchComponent', () => {
-//   let component: GitsearchComponent;
-//   let fixture: ComponentFixture<GitsearchComponent>;
+describe('GitsearchComponent', () => {
+  let component: GitsearchComponent;
+  let fixture: ComponentFixture<GitsearchComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ GitsearchComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GitsearchComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(GitsearchComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GitsearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
