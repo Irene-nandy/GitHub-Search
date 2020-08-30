cc55633a144877f9b1fad3d308e4acbd4d4c8cd2
Irene-nandy <irenekasiva75@gmail.com>
2020-08-30 21:05:56 +0300

    add search.components

--- github/src/app/search/search.component.ts
+++ github/src/app/search/search.component.ts
@@ -1,15 +1,15 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-search',
-//   templateUrl: './search.component.html',
-//   styleUrls: ['./search.component.css']
-// })
-// export class SearchComponent implements OnInit {
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
