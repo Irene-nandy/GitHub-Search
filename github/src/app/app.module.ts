c35107169c1fc283a7ea9aa689dea5ba1ff0f56d
Irene-nandy <irenekasiva75@gmail.com>
2020-08-30 21:08:33 +0300

    add app module

--- github/src/app/app.module.ts
+++ github/src/app/app.module.ts
@@ -1,43 +1,43 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import {FormsModule} from '@angular/forms';
-// import { HttpClientModule} from '@angular/common/http';
-// import { RouterModule, Routes } from '@angular/router';
-// import {RoutingModule} from './routing/routing.module'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import {RoutingModule} from './routing/routing.module'
 
-// import { GitsearcherService } from 'src/app/gitsearcher.service'
-
-
-// import { AppComponent } from './app.component';
-// import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
-// import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
-// import { RepositoriesComponent } from 'src/app/repositories/repositories.component';
-// import { ColourDirective } from './colour.directive';
-// import { DateCountPipe } from './date-count.pipe';
+import { GitsearcherService } from 'src/app/gitsearcher.service'
 
 
-// const routes:Routes=[
-//   {path:"gitsearch",component:GitsearchComponent},
-//   {path:"repositories",component:RepositoriesComponent}
-// ]
+import { AppComponent } from './app.component';
+import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
+import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
+import { RepositoriesComponent } from 'src/app/repositories/repositories.component';
+import { ColourDirective } from './colour.directive';
+import { DateCountPipe } from './date-count.pipe';
 
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     GitsearchFormComponent,
-//     GitsearchComponent,
-//     RepositoriesComponent,
-//     ColourDirective,
-//     DateCountPipe
-//   ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule,
-//     HttpClientModule,
-//     RouterModule.forRoot(routes),
-//     RoutingModule,
-//   ],
-//   providers: [GitsearcherService],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+
+const routes:Routes=[
+  {path:"gitsearch",component:GitsearchComponent},
+  {path:"repositories",component:RepositoriesComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitsearchFormComponent,
+    GitsearchComponent,
+    RepositoriesComponent,
+    ColourDirective,
+    DateCountPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    RoutingModule,
+  ],
+  providers: [GitsearcherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
